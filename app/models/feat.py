8d1023b9a8f1ab9dff26a251f82c269b4fe9771d
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2023-11-20 16:47:59 -0500

    added feats

---
+++ app/models/feat.py
@@ -0,0 +1,11 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+class Feat(db.Model):
+    __tablename__ = 'feats'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(500), nullable=False)
