6c1eca1dbaa5324a2bb6073ae6df49c188ab1889
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2023-11-20 16:56:41 -0500

    fixed error in join table

--- app/models/character_spells.py
+++ app/models/character_spells.py
@@ -4,8 +4,8 @@ character_spells = db.Table(
     'character_spells',
     db.Model.metadata,
     db.Column('id', db.Integer, primary_key=True, autoincrement=True),
-    db.Column('character_id', db.Integer, db.ForeignKey('characters.id')),
-    db.Column('spell_id', db.Integer, db.ForeignKey('spells.id'))
+    db.Column('character_id', db.Integer, db.ForeignKey(add_prefix_for_prod('characters.id'))),
+    db.Column('spell_id', db.Integer, db.ForeignKey(add_prefix_for_prod('spells.id')))
 )
 
 if environment == "production":
