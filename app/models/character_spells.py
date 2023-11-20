d93ad68940421c468b8f6dfbcbbcbe6bd786c963
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2023-11-20 16:55:06 -0500

    join table for character spells

---
+++ app/models/character_spells.py
@@ -0,0 +1,12 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+character_spells = db.Table(
+    'character_spells',
+    db.Model.metadata,
+    db.Column('id', db.Integer, primary_key=True, autoincrement=True),
+    db.Column('character_id', db.Integer, db.ForeignKey('characters.id')),
+    db.Column('spell_id', db.Integer, db.ForeignKey('spells.id'))
+)
+
+if environment == "production":
+    character_spells.schema = SCHEMA
