d1932527c9f57e4ced2937ab7b875a041ba10043
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2023-11-20 16:46:11 -0500

    Item class

---
+++ app/models/item.py
@@ -0,0 +1,15 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+class Item(db.Model):
+    __tablename__ = 'items'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+    type = db.Column(db.String(100), nullable=False)
+    damage = db.Column(db.String(100), nullable=True)
+    weight = db.Column(db.Float, nullable=False)
+    cost = db.Column(db.Float, nullable=False)
