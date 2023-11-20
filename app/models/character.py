8e2b4e9c4cb698c06eaf58d39e8bf8ff7ff8229d
Brian Washington Jr <bw49078@gmail.com>
2023-11-20 15:49:47 -0600

    character now has dict method

--- app/models/character.py
+++ app/models/character.py
@@ -8,9 +8,11 @@ class Character(db.Model):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    # User id foreign key (should we change this to owner id?)
+    ownerId = db.Column(db.Integer, db.ForeignKey(
+        add_prefix_for_prod('users.id')), nullable=False)
+    level = db.Column(db.Integer, nullable=False)
     name = db.Column(db.String(50), nullable=False)
-    race = db.Column(db.String(50), nullable=False)
+    gender = db.Column(db.String(50), nullable=False)
     description = db.Column(db.String(500), nullable=True)
     hit_points = db.Column(db.Integer, nullable=False)
     strength = db.Column(db.Integer, nullable=False)
@@ -19,7 +21,28 @@ class Character(db.Model):
     intelligence = db.Column(db.Integer, nullable=False)
     wisdom = db.Column(db.Integer, nullable=False)
     charisma = db.Column(db.Integer, nullable=False)
-    skills = db.Column(db.String(50), nullable=True)
-    spell_slot = db.Column(db.String(50), nullable=True)
+    #skills = db.Column(db.String(50), nullable=True)
+    #spell_slot = db.Column(db.String(50), nullable=True)
+    #race = db.Column(db.String(50), nullable=False)
+    #race = db.Column(db.String(50), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.now())
     updated_at = db.Column(db.DateTime, default=datetime.now())
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'ownerId': self.content,
+            'level': self.level,
+            'name': self.name,
+            'gender': self.gender,
+            'description': self.description,
+            'hit_points': self.hit_points,
+            'strength': self.strength,
+            'dexterity': self.dexterity,
+            'constitution': self.constitution,
+            'intelligence': self.intelligence,
+            'wisdom': self.wisdom,
+            'charisma': self.charisma,
+            'createdAt': self.createdAt,
+            'updatedAt': self.updatedAt,
+        }
