4c767f6674cfa0573b21a89e6016dc869af090a8
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2023-11-20 16:37:06 -0500

    skill to dict

--- app/models/skill.py
+++ app/models/skill.py
@@ -9,3 +9,10 @@ class Skill(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     description = db.Column(db.String(500), nullable=True)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description
+        }
