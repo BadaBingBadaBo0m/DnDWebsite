857c0ac309f9b28b2ea728d56f0762d9b669fc77
Brian Washington Jr <bw49078@gmail.com>
2023-11-20 21:02:53 -0600

    added inventory join table

---
+++ app/models/character_inventory.py
@@ -0,0 +1,14 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+character_inventory = db.Table(
+    'character_inventory',
+    db.Model.metadata,
+    db.Column('id', db.Integer, primary_key=True, autoincrement=True),
+    db.Column('character_id', db.Integer, db.ForeignKey(add_prefix_for_prod('characters.id'))),
+    db.Column('item_id', db.Integer, db.ForeignKey(add_prefix_for_prod('items.id'))),
+    db.Column('quantity', db.String(10), nullable=False),
+    db.Column('equipped', db.Boolean, nullable=False)
+)
+
+if environment == "production":
+    character_skills.schema = SCHEMA
