fd8ba1bd68d5e85f0b6686a621a0b4b6df42e89d
Brian Washington Jr <bw49078@gmail.com>
2023-11-22 16:48:21 -0600

    create character backend route now created

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -66,7 +66,7 @@ def seed():
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
-    # undo_character_classes()
+    undo_character_classes()
     undo_character_inventories()
     undo_campaign_characters()
     undo_character_feats()
