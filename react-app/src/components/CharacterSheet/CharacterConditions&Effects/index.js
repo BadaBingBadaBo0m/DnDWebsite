b554a492d1f3e492b421f85d71902c1b245917dd
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2023-11-22 18:59:04 -0500

    character death saves roughly in place - before adding font aweseome

--- react-app/src/components/CharacterSheet/CharacterConditions&Effects/index.js
+++ react-app/src/components/CharacterSheet/CharacterConditions&Effects/index.js
@@ -7,21 +7,43 @@ const CharacterConditionsAndEffects = () => {
     <div id='character-conditions-and-effects-container'>
       <div id='character-hitDice-armorClass-deathSave-container'>
         <div id='character-hit-dice-container'>
-          <div className='hit-die max'>
-            <h2 className='character-sheet-h2'>MAX</h2>
-            <p>1d8</p>
+
+          <div id='hit-die-container'>
+            <div className='hit-die max'>
+              <h2 className='character-sheet-h2'>MAX</h2>
+              <p>1d8</p>
+            </div>
+
+            <div className='hit-die current'>
+              <h2 className='character-sheet-h2'>CURRENT</h2>
+              <p>1d8</p>
+            </div>
           </div>
 
-          <div className='hit-die current'>
-            <h2 className='character-sheet-h2'>CURRENT</h2>
-            <p>1d8</p>
-          </div>
+          <h2 className='character-sheet-h2'>HIT DICE</h2>
         </div>
 
-        <h2>HIT DICE</h2>
+        <div id='character-armor-class'>
+          <p>16</p>
+          <h2 className='character-sheet-h2'>ARMOR</h2>
+          <h2 className='character-sheet-h2'>CLASS</h2>
+        </div>
+
+        <div id='character-death-saves'>
+          <div id='death-save-failure'>
+            <h2 className='character-sheet-h2'>FAILURE</h2>
+          </div>
+
+          <div id='death-save-success'>
+            <h2 className='character-sheet-h2'>SUCCESS</h2>
+          </div>
+
+          <h2 className='character-sheet-h2'>DEATH SAVES</h2>
+        </div>
       </div>
 
     </div>
+
   );
 }
 
