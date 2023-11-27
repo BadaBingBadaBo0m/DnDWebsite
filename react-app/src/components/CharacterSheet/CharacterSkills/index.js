185d338b82d7c4c0e28a3cda95c1d4898a046c71
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2023-11-27 16:19:38 -0500

    skills in place

--- react-app/src/components/CharacterSheet/CharacterSkills/index.js
+++ react-app/src/components/CharacterSheet/CharacterSkills/index.js
@@ -1,10 +1,122 @@
 import React from 'react';
+import './characterSkills.css';
 
 const CharacterSkills = () => {
 
   return (
-    <div>
-      <h1>Skills</h1>
+    <div id='character-skills-tree'>
+      <ul>
+        <li className='character-skills-li'>
+          <i class="fa-regular fa-circle"></i>
+          <p>1</p>
+          <p>Acrobatics (DEX)</p>
+        </li>
+
+        <li className='character-skills-li'>
+          <i class="fa-regular fa-circle"></i>
+          <p>2</p>
+          <p>Animal Handling (WIS)</p>
+        </li>
+
+        <li className='character-skills-li'>
+          <i class="fa-regular fa-circle"></i>
+          <p>3</p>
+          <p>Arcana (INT)</p>
+        </li>
+
+        <li className='character-skills-li'>
+          <i class="fa-regular fa-circle"></i>
+          <p>4</p>
+          <p>Athletics (STR)</p>
+        </li>
+
+        <li className='character-skills-li'>
+          <i class="fa-regular fa-circle"></i>
+          <p>5</p>
+          <p>Deception (CHA)</p>
+        </li>
+
+        <li className='character-skills-li'>
+          <i class="fa-regular fa-circle"></i>
+          <p>6</p>
+          <p>History (INT)</p>
+        </li>
+
+        <li className='character-skills-li'>
+          <i class="fa-regular fa-circle"></i>
+          <p>7</p>
+          <p>Insight (WIS)</p>
+        </li>
+
+        <li className='character-skills-li'>
+          <i class="fa-regular fa-circle"></i>
+          <p>8</p>
+          <p>Intimidation (CHA)</p>
+        </li>
+
+        <li className='character-skills-li'>
+          <i class="fa-regular fa-circle"></i>
+          <p>9</p>
+          <p>Investigation (INT)</p>
+        </li>
+
+        <li className='character-skills-li'>
+          <i class="fa-regular fa-circle"></i>
+          <p>1</p>
+          <p>Medicine (WIS)</p>
+        </li>
+
+        <li className='character-skills-li'>
+          <i class="fa-regular fa-circle"></i>
+          <p>1</p>
+          <p>Nature (INT)</p>
+        </li>
+
+        <li className='character-skills-li'>
+          <i class="fa-regular fa-circle"></i>
+          <p>1</p>
+          <p>Perception (WIS)</p>
+        </li>
+
+        <li className='character-skills-li'>
+          <i class="fa-regular fa-circle"></i>
+          <p>1</p>
+          <p>Performance (CHA)</p>
+        </li>
+
+        <li className='character-skills-li'>
+          <i class="fa-regular fa-circle"></i>
+          <p>1</p>
+          <p>Persuasion (CHA)</p>
+        </li>
+
+        <li className='character-skills-li'>
+          <i class="fa-regular fa-circle"></i>
+          <p>1</p>
+          <p>Religion (INT)</p>
+        </li>
+
+        <li className='character-skills-li'>
+          <i class="fa-regular fa-circle"></i>
+          <p>1</p>
+          <p>Sleight of Hand (DEX)</p>
+        </li>
+
+        <li className='character-skills-li'>
+          <i class="fa-regular fa-circle"></i>
+          <p>1</p>
+          <p>Stealth (DEX)</p>
+        </li>
+
+        <li className='character-skills-li'>
+          <i class="fa-regular fa-circle"></i>
+          <p>1</p>
+          <p>Survival (WIS)</p>
+        </li>
+
+      </ul>
+
+      <h2 className='character-sheet-h2'>SKILLS</h2>
     </div>
   );
 };
