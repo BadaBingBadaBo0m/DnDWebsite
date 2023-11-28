f23a3118f2d2d5621fd61bf8708d3e3a2bd6b210
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2023-11-28 03:28:39 -0500

    making character sheet modular

--- react-app/src/components/CharacterSheet/index.js
+++ react-app/src/components/CharacterSheet/index.js
@@ -19,7 +19,7 @@ const CharacterSheet = () => {
 
   useEffect(() => {
     (async () => {
-      const res = await dispatch(getCharacterById(1));
+      const res = await dispatch(getCharacterById(3));
 
       if (res.errors) {
         setErrors(res);
