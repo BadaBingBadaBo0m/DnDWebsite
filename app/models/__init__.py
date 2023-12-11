6e2da86bca530a41a8b43a9041f0a7318e87e8f4
Brian Washington Jr <bw49078@gmail.com>
2023-12-10 18:06:44 -0600

    created spell and skill model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,6 @@
 from .db import db
 from .user import User
 from .character import Character
+from .spell import Spell
+from .skill import Skill
 from .db import environment, SCHEMA
---
+++ app/models/spell.py
@@ -0,0 +1,33 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+import datetime
+
+class Spell(db.Model):
+    __tablename__ = 'spells'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(40), nullable=False, unique=True)
+    descripton = db.Column(db.String(255), nullable=False, unique=True)
+    range = db.Column(db.String(255), nullable=False, unique=True)
+    duration = db.Column(db.String(255), nullable=False, unique=True)
+    components = db.Column(db.String(255), nullable=False, unique=True)
+    ritual = db.Column(db.Boolean, nullable=False, unique=True)
+    concentration = db.Column(db.Boolean, nullable=False, unique=True)
+    casting_time = db.Column(db.String(255), nullable=False, unique=True)
+    damage_at_slot_level = db.Column(db.String(255), nullable=False, unique=True)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'range': self.range,
+            'duration': self.duration,
+            'components': self.components,
+            'ritual': self.ritual,
+            'concentration': self.concentration,
+            'casting_time': self.casting_time,
+            'damage_at_slot_level': self.damage_at_slot_level
+        }
