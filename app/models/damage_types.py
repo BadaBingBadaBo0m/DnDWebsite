a754b23c4589262970f313109c16febcd877a9f2
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2023-12-13 15:54:55 -0500

    damage types table

--- app/models/damage_types.py
+++ app/models/damage_types.py
@@ -1,7 +1,7 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 
-class Language(db.Model):
-    __tablename__ = 'language'
+class Damage_Types(db.Model):
+    __tablename__ = 'damages_types'
 
     if environment == 'production':
         __table_args__ = {'schema': SCHEMA}
